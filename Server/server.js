c8a748262f9e0ddaba0c09c9a1fdecec83b6df73
lahiruchalana <lahiruchalanairesh@gmail.com>
2022-10-27 21:39:32 +0530

    completed server and mongoDb connection

---
+++ Server/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 9090;
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB successfully connected!!!");
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on : ${PORT}`)
+})
